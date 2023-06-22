dc44a2af98060f2b812b568d6779ae47a8c014cb
autogalkin <galigorrom@gmail.com>
2023-06-22 19:49:46 +0300

    build: add dependencies
    - add clap, ratatui, crossterm, rand
    - start main function...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::{arg, command};
+use std::{
+    io::{self, Stdout}
+    , time::Duration
+    , error::Error
+    }
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode}
+    , execute
+    , terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}
+}
+use ratatui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout, Alignment},
+    widgets::{Block, Borders, Paragraph},
+    Frame, Terminal,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        // Args here
+        /*        .arg(
+            arg!(
+                -c --config <FILE> "Sets a custom config file"
+            )
+            .required(false)
+        )
+        .arg(arg!(
+            -t --text <TEXT> "Text to print"
+        ))
+        */ 
+        .get_matches();
+
+    
+
 }
